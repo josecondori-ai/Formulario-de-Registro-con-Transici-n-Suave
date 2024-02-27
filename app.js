55194a39b494a3ea43b4a76c3e50fdf73a3360ff
jose luis condori <jose.l.condori@gmail.com>
2024-02-27 18:07:30 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const container=document.querySelector(".container")
+const signupButton=document.querySelector(".fondo button")
+
+signupButton.addEventListener("click",()=>{
+    container.classList.toggle("change")
+})
